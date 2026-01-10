d92b028f0f853d0d93f7634eb45fe160c3793862
Patricio Frettes Bazante <130622561+patriciofrettes@users.noreply.github.com>
2026-01-10 01:01:47 -0300

    Codigo seguro usando User Secrets

--- TaraguyAPI/TaraguyAPI/Services/ImagenService.cs
+++ TaraguyAPI/TaraguyAPI/Services/ImagenService.cs
@@ -1,6 +1,7 @@
 using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration; // Necesario para leer config
 using System;
 using System.IO;
 using System.Threading.Tasks;
@@ -9,36 +10,35 @@ namespace TaraguyAPI.Services
 {
     public class ImagenService
     {
-        // ⚠️ PEGÁ ACÁ LA CADENA DE CONEXIÓN QUE COPIASTE DE AZURE
-        private readonly string _connectionString = "DefaultEndpointsProtocol=https;AccountName=storagetaraguy2026;AccountKey=Yjhhranbub2U4/qL0zwZkA0nOEZ//ovHMHlGjvV+oYvzOLn8ouZBSPNVdjWEHpMgr+01947PjecT+AStLsorWg==;EndpointSuffix=core.windows.net";
-        
+        private readonly string _connectionString;
         private readonly string _containerName = "imagenes";
+
+        // Inyectamos IConfiguration para leer la clave de forma segura
+        public ImagenService(IConfiguration configuration)
+        {
+            // Busca una variable llamada "AzureStorageConnection"
+            _connectionString = configuration.GetConnectionString("AzureStorageConnection");
+        }
 
         public async Task<string> SubirImagenAsync(IFormFile archivo)
         {
             if (archivo == null || archivo.Length == 0) return null;
 
-            try 
+            try
             {
-                // Conectamos a Azure
                 var blobServiceClient = new BlobServiceClient(_connectionString);
                 var containerClient = blobServiceClient.GetBlobContainerClient(_containerName);
-                
-                // Creamos el contenedor si no existe (por las dudas)
                 await containerClient.CreateIfNotExistsAsync(PublicAccessType.Blob);
 
-                // Nombre único para la foto
                 string extension = Path.GetExtension(archivo.FileName);
                 string nombreArchivo = Guid.NewGuid().ToString() + extension;
 
-                // Subimos la foto
                 var blobClient = containerClient.GetBlobClient(nombreArchivo);
                 using (var stream = archivo.OpenReadStream())
                 {
                     await blobClient.UploadAsync(stream, new BlobHttpHeaders { ContentType = archivo.ContentType });
                 }
 
-                // Devolvemos la URL directa de internet (ej: https://taraguy.blob.core.../foto.jpg)
                 return blobClient.Uri.ToString();
             }
             catch (Exception ex)
